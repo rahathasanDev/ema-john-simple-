610f1f154976868449ea23e8986f0b6d84a1f37c
Rahat Hasan <devrahat.hasan@gmail.com>
2023-11-15 11:08:53 +0600

    header added

---
+++ src/components/header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './header.css';
+import logo from '../images/Logo.svg'
+
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <img src={logo} alt="" />
+      <div className='navbar'>
+        <a href="/Shop">Shop</a>
+        <a href="/Order">Order</a>
+        <a href="/Inventory">Inventory</a>
+        <a href="/login">login</a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
